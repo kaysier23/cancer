62f5bca72eba8579e4689105b573c3b71ef1af65
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-03-21 21:20:12 +0800

    Create config.php

---
+++ google/config.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$google_client = new Google_Client();
+
+$google_client->setClientId('831814022834-hag6q8pvc4vasdppihs0ppvdgfa8j676.apps.googleusercontent.com');
+
+$google_client->setClientSecret('NxduNzGGdDeIpzvkZJsX42jN');
+
+$google_client->setRedirectUri('https://ehzdf.herokuapp.com/');
+
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+session_start();
+
+?>
