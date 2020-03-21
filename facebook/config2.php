790b1b0a993ecc4a704482e6c0851176431a4cd1
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-03-21 22:10:03 +0800

    Update config2.php

--- facebook/config2.php
+++ facebook/config2.php
@@ -10,8 +10,8 @@ if (!session_id())
 // Call Facebook API 
 
 $facebook = new \Facebook\Facebook([
-  'app_id'      => '206238130687793',
-  'app_secret'     => '438a0d88ded7c92926e81821d6c39202',
+  'app_id'      => '240980820422533',
+  'app_secret'     => 'dfe86e961869a27e388a3de7770d9d15',
   'default_graph_version'  => 'v2.10'
 ]);
 
